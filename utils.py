4cf54aae41c9f76823a188b5adadfd6ba0080de1
Kuba PrusakiewiczPC <jakubprusakiewicz@gmail.com>
2021-01-02 16:04:31 +0100

    feat missing type hints

--- utils.py
+++ utils.py
@@ -2,11 +2,11 @@ from itertools import groupby
 from typing import List
 
 
-def group_by_identical_digits(digits_list) -> List[List[int]]:
+def group_by_identical_digits(digits_list: List[int]) -> List[List[int]]:
     return [list(g) for k, g in groupby(digits_list)]
 
 
-def list_to_chunks(digits, chunk_len=2):
+def list_to_chunks(digits: List[int], chunk_len=2) -> List[List[int]]:
     step = 1
     chunks = []
     for i in range(0, len(digits) - chunk_len + step, step):
@@ -15,5 +15,5 @@ def list_to_chunks(digits, chunk_len=2):
     return chunks
 
 
-def number_to_digits_list(number) -> List[int]:
+def number_to_digits_list(number: int) -> List[int]:
     return [int(d) for d in str(number)]
